3453d07039d5851251493ca806269f5356f706e6
danviana-has <danviana@gmail.com>
2026-05-23 15:46:34 -0300

    Set hostname=gmos in kernel command line to load the apkovl configuration overlay at boot

--- gmos-builder/mkimg.gmos.sh
+++ gmos-builder/mkimg.gmos.sh
@@ -7,7 +7,7 @@ profile_gmos() {
     profile_standard
     apks="$apks alpine-base xfce4 xfce4-terminal lightdm lightdm-gtk-greeter dbus xorg-server xf86-video-modesetting xf86-video-fbdev xf86-video-vesa xf86-input-libinput eudev sudo nano wget curl bash htop font-dejavu shadow mesa-gl mesa-dri-gallium"
     kernel_flavors="lts"
-    kernel_cmdline="unionfs_size=512M console=tty0"
+    kernel_cmdline="unionfs_size=512M console=tty0 hostname=gmos"
     syslinux_serial="0 115200"
     apkovl="genapkovl-gmos.sh"
 }

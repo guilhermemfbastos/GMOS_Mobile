ec09cbaf45a7a321df39ae56ac35462b849a5500
danviana-has <danviana@gmail.com>
2026-05-23 18:24:00 -0300

    Increase VM memory to 4GB and CPU cores to 4 for better performance

--- run_gmos.sh
+++ run_gmos.sh
@@ -84,8 +84,8 @@ log_info "Iniciando QEMU em background..."
 rm -f qemu_boot.log
 
 qemu-system-x86_64 \
-  -m 2048 \
-  -smp 2 \
+  -m 4096 \
+  -smp 4 \
   $QEMU_ACCEL \
   -boot d \
   -cdrom "$ISO_PATH" \
